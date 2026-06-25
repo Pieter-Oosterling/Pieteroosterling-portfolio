494a5bd2ecdcee2784cef1555a8cf7e31afba79c
Nvt-Test-NL <saucisse-lijn8y@icloud.com>
2026-06-25 13:28:14 +0200

    cijfer

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -842,7 +842,7 @@ Het gekozen doublet-scenario "Buurten en Wijken" levert 250 TJ warmte per jaar m
         description: 'Een medische reddingsdrone voor het slagveld.',
         client: 'Ministerie van Defensie \n (Bart de Graaff)',
         date: '01-05-2026',
-        grade: {},
+        grade: { group: 'ntb', personal: 'ntb' }, // Grade pending
         team: ['Mohammad Khaled Yasin', 'Felix Roeterdink', 'Pepijn de Graaff', 'Pieter Oosterling'],
         role: 'Onderzoeker & Ontwerper',
         tags: ['Onderzoek', 'Design', 'Tech'],
